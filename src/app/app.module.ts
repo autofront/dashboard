f22f3a43ef6a708ff8e4362b2cdcfb003d76b530
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 13:18:54 -0300

    :heavy_plus_sign: added bootstrap framework for visual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule, AppComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ...AppComponents
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, ...AppComponents],
+	imports: [BrowserModule, AppRoutingModule, NgbModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
