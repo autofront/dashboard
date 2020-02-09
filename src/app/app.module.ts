d97a0dc354e940c09374767d24df32803c94f01e
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 16:35:21 -0300

    :art: module forms and object default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule, AppComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { SharedComponents } from './shared/shared.module';
 
 @NgModule({
 	declarations: [AppComponent, ...AppComponents, ...SharedComponents],
-	imports: [BrowserModule, AppRoutingModule, NgbModule],
+	imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+	component = {
+		form: null,
+		formInputs: {
+			apiUrl: {
+				defaultValue: 'https://protected-ridge-35353.herokuapp.com/',
+				disabled: true,
+				value: 'Lorem ipsum',
+			},
+		},
+		data: null,
+		render: {
+			class: {
+				largeMdSm: 'col-lg-6 col-md-12',
+			},
+		},
+	};
+
+	
+}
