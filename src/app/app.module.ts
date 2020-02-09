5c10d250243704f2aa215de545d1b93ec59c3e31
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 17:19:01 -0300

    :heavy_plus_sign: install and settings NgX Spinner for loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 import { AppRoutingModule, AppComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { SharedComponents } from './shared/shared.module';
 
 @NgModule({
 	declarations: [AppComponent, ...AppComponents, ...SharedComponents],
-	imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
+	imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule, NgxSpinnerModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
