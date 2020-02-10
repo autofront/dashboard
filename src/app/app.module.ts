ca8e3072665fc233d6b0305c692ccac415f6a7a7
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 21:41:28 -0300

    :zap: added HttpClient and create service for default http Requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { NgxSpinnerModule } from "ngx-spinner";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule, AppComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,15 @@ import { SharedComponents } from './shared/shared.module';
 
 @NgModule({
 	declarations: [AppComponent, ...AppComponents, ...SharedComponents],
-	imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule, NgxSpinnerModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		NgbModule,
+		FormsModule,
+		NgxSpinnerModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
