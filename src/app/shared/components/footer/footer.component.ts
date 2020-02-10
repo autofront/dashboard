0d851deba739f6b48622d62b3122e82796860160
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 23:53:07 -0300

    :art: added footer

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const SharedComponents = [HeaderComponent];
+export const SharedComponents = [FooterComponent, HeaderComponent];
